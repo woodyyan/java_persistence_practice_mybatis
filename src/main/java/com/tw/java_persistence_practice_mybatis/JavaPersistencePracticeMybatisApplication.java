e6b93265a11b87d86a9fa2e888ba7bf81578fe74
Songbai Yan <colorguitar@hotmail.com>
2019-08-25 17:19:03 +0800

    first commit

---
+++ src/main/java/com/tw/java_persistence_practice_mybatis/JavaPersistencePracticeMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.tw.java_persistence_practice_mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPersistencePracticeMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPersistencePracticeMybatisApplication.class, args);
+	}
+
+}
